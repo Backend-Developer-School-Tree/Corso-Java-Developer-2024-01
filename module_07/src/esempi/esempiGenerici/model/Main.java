375236b2dec23ed6563a5172f5aed75e8a01959d
Davide Fella <davide.fella@gmail.com>
2024-03-05 11:16:03 +0100

    Live Coding Generics

--- module_07/src/esempi/esempiGenerici/model/Main.java
+++ module_07/src/esempi/esempiGenerici/model/Main.java
@@ -1,8 +1,15 @@
 package esempi.esempiGenerici.model;
 
+import esempi.esempiLista.Dipendente;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+
+/*
+* LINK UTILI: https://www.baeldung.com/java-instanceof
+*
+* */
 
 public class Main {
 
@@ -30,5 +37,30 @@ public class Main {
             }
         }
 
+        List<Integer> myCollectionWithGenerics = new ArrayList<>();
+        myCollectionWithGenerics.add(42);
+
+        for(int i=0; i<100; i++){
+            myCollectionWithGenerics.add(i);
+            // errore a tempo statico perché sto aggiungedo una stringa myCollectionWithGenerics.add("ciao");
+        }
+
+        // best practice --> nomi al plurale se abbiamo collezioni
+        List<Person> persons = new ArrayList<>();
+        persons.add(new Student()); // Student eredita Person, quindi posso aggiungerla
+        // errore perché Dipendente non estende persona - persons.add(new Dipendente(0, "Nome", 25, 500.00)); // Student eredita Person, quindi posso aggiungerla
+
+        for (Person p : persons){
+            System.out.println(p);
+
+            Student sCasted = (Student) p;
+            sCasted.methodOnlyStudent();
+        }
+
+        // Container vuole una T,S generici (ricorda NO TIPI PRIMITIVI)
+        Container<Integer, String> c1 = new Container<>(0, "S1");
+
+        List<Container<Integer, String>> containers = new ArrayList<>();
+        containers.add(c1);
     }
 }
