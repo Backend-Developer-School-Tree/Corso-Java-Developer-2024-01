cefc083074dc5354a6d33a934d60e26b556309c4
Davide Fella <davide.fella@gmail.com>
2024-03-14 10:58:58 +0100

    Connection Database

--- module_09/src/App.java
+++ module_09/src/App.java
@@ -1,14 +1,26 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class App {
 
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        // Credenziali di PostgreSQL.
+        // TODO: Usare classe Properties di Java
+        String username = "postgres";
+        String password = "postgres";
+        String urlConnection = "jdbc:postgresql://localhost:5432/movie_platform";
 
         /*
         Step 0 --> Carico il Driver in memoria, in teoria non più necessario nelle nuove versioni
         Per chi ha MySQL: https://dev.mysql.com/doc/connector-j/en/connector-j-usagenotes-connect-drivermanager.html
         */
         Class.forName("org.postgresql.Driver");
+        Connection conn = DriverManager.getConnection(urlConnection, username, password);
 
-        System.out.println("Hello World!");
+
+        System.out.println("Connessione stabilita! Schema: " + conn.getSchema());
 
     }
 }
