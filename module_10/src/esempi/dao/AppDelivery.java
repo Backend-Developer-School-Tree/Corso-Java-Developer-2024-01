af6bfe0b58baf7b372920db0d3549318ca9837e0
Andrea Gasparini <andrea.gasparini11@gmail.com>
2024-03-18 14:47:42 +0100

    Update AppDelivery.java

--- module_10/src/esempi/dao/AppDelivery.java
+++ module_10/src/esempi/dao/AppDelivery.java
@@ -11,10 +11,12 @@ public class AppDelivery
         // aggiungo controlli relativi alla logica di business
         // es. controllo se già esiste un utente con lo stesso id
         // nel caso di UserDaoSQL getUser farà una query del tipo "SELECT FROM utenti WHERE id = u.id"
+        // nel caso di UserDaoColleciton invece farà una semplice ricerca dentro una lista o una mappa
         if (userDao.getUser(u.id) == null)
         {
             // se non esiste un utente lo creo
             // nel caso di UserDaoSQL insertUser farà una query del tipo "INSERT INTO ..."
+            // nel caso di UserDaoCollection invece farà un semplice inserimento dentro una lista o una mappa
             userDao.insertUser(u);
         }
     }
