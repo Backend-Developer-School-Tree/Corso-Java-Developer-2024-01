d557979b7de5e2e230c03ee23f2776246d074d92
Davide Fella <davide.fella@gmail.com>
2024-03-27 09:56:54 +0100

    Arcade Rank Spring Init

---
+++ module_13/src/esempi/opinnoarcaderankspring/src/main/java/com/opinno/arcaderank_spring/OpinnoarcaderankspringApplication.java
@@ -0,0 +1,13 @@
+package com.opinno.arcaderank_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpinnoarcaderankspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpinnoarcaderankspringApplication.class, args);
+	}
+
+}
---
+++ module_13/src/esempi/opinnoarcaderankspring/src/test/java/com/opinno/arcaderank_spring/OpinnoarcaderankspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opinno.arcaderank_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpinnoarcaderankspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
